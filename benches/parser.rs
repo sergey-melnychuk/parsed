2722aa16837ed77839aef295007ad6392f2984f1
sergey-melnychuk <sergey-melnychuk@users.noreply.github.com>
2020-05-05 20:10:26 +0200

    Add simple benchmark for HTTP request parser

---
+++ benches/parser.rs
@@ -0,0 +1,22 @@
+#![feature(test)]
+#[cfg(test)]
+
+use parser_combinators::stream::ToStream;
+use parser_combinators::parser::Applicator;
+use parser_combinators::http::parse_http_request;
+
+extern crate test;
+use test::Bencher;
+
+// rustup install nightly
+// rustup run nightly cargo bench
+
+#[bench]
+fn bench_parse_http_request(b: &mut Bencher) {
+    let text = "GET /chat HTTP/1.1\r\nHost: example.com:8000\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\nSec-WebSocket-Version: 13\r\n\r\n";
+    b.iter(|| {
+        let mut bs = text.to_string().into_stream();
+        let req = parse_http_request(&mut bs);
+        assert!(req.is_ok());
+    });
+}
