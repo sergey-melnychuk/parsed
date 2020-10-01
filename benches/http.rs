2356d98f85c0c91d961b1f1fdb78cd0c1d3bba48
sergey-melnychuk <sergey-melnychuk@users.noreply.github.com>
2020-10-01 23:04:04 +0200

    Add ws_frame parser benchmark

--- benches/http.rs
+++ benches/http.rs
@@ -4,8 +4,9 @@ use bencher::Bencher;
 
 use parsed::stream::ByteStream;
 use parsed::http::parse_http_request;
+use parsed::ws::parse_frame;
 
-fn bench_parse_http_request(b: &mut Bencher) {
+fn http_request(b: &mut Bencher) {
     let text = "GET /chat HTTP/1.1\r\nHost: example.com:8000\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\nSec-WebSocket-Version: 13\r\n\r\n";
     b.iter(|| {
         let mut bs: ByteStream = text.to_string().into();
@@ -14,5 +15,16 @@ fn bench_parse_http_request(b: &mut Bencher) {
     });
 }
 
-benchmark_group!(http, bench_parse_http_request);
+fn ws_frame(b: &mut Bencher) {
+    let bytes: Vec<u8> = vec![129, 134, 87, 35, 230, 82, 63, 70, 138, 62, 56, 2];
+    b.iter(|| {
+        let mut bs = ByteStream::wrap(bytes.clone());
+        let opt = parse_frame(&mut bs);
+        assert!(opt.is_some());
+    });
+}
+
+
+
+benchmark_group!(http, http_request, ws_frame);
 benchmark_main!(http);
